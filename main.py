d90ea288e7892b873468d59f1e3c3856657ac4ac
Filip Ziarko <93010881+f3xeek@users.noreply.github.com>
2024-08-25 11:21:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import ast
 import numpy as np
+from datetime import datetime,timedelta 
 
 #constants
 G=6.6743e-11
@@ -10,20 +11,25 @@ timespan_in_seconds=3600
 initial_scale=1_000_000_000
 
 class Symulator:
-    def __init__(self,surface:pygame.Surface,scale:int=initial_scale, background:tuple[int,int,int]=(0,0,0)) -> None:
-        self.elements=[]
+    def __init__(self,surface:pygame.Surface,date:datetime,elements:list,scale:int=initial_scale, background:tuple[int,int,int]=(0,0,0)) -> None:
+        self.elements=elements
         self.background=background
         self.surface=surface
         self.scale=scale
-
+        self.date = date
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
 # dictates what happenes each frame during the simulation, calculates position of elements
-    def frame(self,screen_size) -> None:
+    def frame(self,screen_size, Draw_date:bool, time=timespan_in_seconds) -> None:
         for element in self.elements:
             pos=element.position/self.scale+screen_size/2
             if initial_scale/self.scale*element.size>=1:
                 pygame.draw.circle(self.surface, element.color, [pos[0],screen_size-pos[1]], element.size*initial_scale/self.scale,0)
             else:
                 pygame.draw.circle(self.surface, element.color, [pos[0],screen_size-pos[1]], 1,0)
+        if Draw_date:
+            self.date=self.date+timedelta(seconds=time)
+            text = self.font.render(str(self.date)[:11],True,(255,255,255))
+            self.surface.blit(text, (0,0))
 
 class Planet:
     def __init__(self,name:str, mass:float, velocity:np.array, position:np.array,color:tuple[int,int,int]=(0,0,255), size:int=3) -> None:
@@ -49,39 +55,48 @@ class Planet:
                 self.acceleration+=force/self.mass
         self.velocity+=self.acceleration*time
 
-    @staticmethod
-    def load_from_file(file)-> list:
-        output=[]
-        with open(file) as file:
-            for line in file.readlines():
-                name,mass,velocity,position,color,size=line.split()
-                output.append(Planet(name,float(mass),np.array(ast.literal_eval(velocity)),np.array(ast.literal_eval(position)),ast.literal_eval(color),int(size)))
-        return output
+def load_from_file(file)-> list:
+    output=[]
+    with open(file) as file:
+        for index,line in enumerate(file.readlines()):
+            if index == 0:
+                date=ast.literal_eval(line)
+                continue
+            name,mass,velocity,position,color,size=line.split()
+            output.append(Planet(name,float(mass),np.array(ast.literal_eval(velocity)),np.array(ast.literal_eval(position)),ast.literal_eval(color),int(size)))
+    return datetime(*date),output
     
-def main(file) ->None:
+def main(file) -> None:
     #Initial start up
     pygame.init()
     screen = pygame.display.set_mode((screen_size, screen_size))
-    symulator=Symulator(screen)
-    symulator.elements=Planet.load_from_file(file)
+    date,elements=load_from_file(file)
+    symulator=Symulator(screen,date,elements)
     #pygame's window utility, scrolling and keypress
     running = True
+    stop = False
+    i=0
     while running:
+        i+=1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key==pygame.K_r:
                     symulator.elements=Planet.load_from_file(file)
+                if event.key == pygame.K_SPACE:
+                    stop=not stop
             if event.type == pygame.MOUSEWHEEL:
                 if symulator.scale >initial_scale/10 or -event.y==1:
                     symulator.scale+=initial_scale/10*-event.y
         #simulating, drawing on screen
-        screen.fill(symulator.background)
-        for element in symulator.elements:     
-            element.change_over_time(timespan_in_seconds, symulator.elements)
-        symulator.frame(screen_size)
+        if not stop:
+            screen.fill(symulator.background)
+            for element in symulator.elements:     
+                element.change_over_time(timespan_in_seconds, symulator.elements)
+            symulator.frame(screen_size, True)
+        
         pygame.display.update()
 
 if __name__=="__main__":
-    main("data22_03_24.txt")
+    main("E:/Programowanie/pajton/planets/data22_03_24.txt")
